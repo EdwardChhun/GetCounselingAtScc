71dc23077dd59048edab4f27cfa5d63aa1bc3c96
Edward Chhun <edwardchhun3@gmail.com>
2024-05-02 01:37:15 -0700

    Added base templates for webscraping

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Making In-person appointments only for now at SCC
+Website : "https://esars.scc.losrios.edu/esars/counseling_ip/eSARS.asp?WCI=Init&WCE=Settings"
+Basics: Making an appointment for [Consortium]
+To Do: Scrape the data and search for "Counselors" and "Times available"
+"""
+import requests 
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+# Making a GET request 
+r = requests.get('https://www.geeksforgeeks.org/python-programming-language/') 
+  
+# check status code for response received 
+# success code - 200 
+print(r) 
+print(".... \n")
+# Parsing the HTML
+soup = BeautifulSoup(r.content, 'html.parser') 
+
+s = soup.find('div', class_='entry-content')
+
+# This will find all the tags
+content = s.find_all('p')
+
+
+print(content)
