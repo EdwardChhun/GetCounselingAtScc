88c9ad5f0ae3c4a568269bb27fad615c323454a3
Edward Chhun <edwardchhun3@gmail.com>
2024-05-03 00:38:22 -0700

    Changed imports

--- webscrape.py
+++ webscrape.py
@@ -1,18 +1,8 @@
 # importing necessary packages 
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
-from webdriver_manager.chrome import ChromeDriverManager 
-import selenium.webdriver.chrome.webdriver
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import pyautogui
-from webdriver_manager.chrome import ChromeDriverManager
-import random
-import numpy as np
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
 # options = Options()
 # options.add_experimental_option("detach", True)
 # driver = webdriver.Chrome(options=options)
@@ -28,11 +18,12 @@ driver = webdriver.Chrome(options=options)
 driver.get(page_url) 
 title = driver.find_elements(By.CLASS_NAME, "title") 
 price = driver.find_elements(By.CLASS_NAME, "price") 
-description = driver.find_elements(By.CLASS_NAME, "description") 
+description = driver.find_elements(By.CLASS_NAME, "description")    
 rating = driver.find_elements(By.CLASS_NAME, "ratings") 
+review_count = driver.find_elements(By.CLASS_NAME, "review-count")
 
-for i in range(len(title)): 
-    element_list.append([title[i].text, price[i].text, description[i].text, rating[i].text]) 
+for i in range(len(title)):
+    element_list.append([title[i].text, price[i].text, description[i].text, rating[i].text, review_count[i].text]) 
 
 print(element_list) 
 
